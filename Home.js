e74ad035b447b0870bb5afb1bb581304a3122242
HamedMiri72 <160745852+HamedMiri72@users.noreply.github.com>
2024-07-04 02:33:51 +0330

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,9 +1,21 @@
+import { editableInputTypes } from '@testing-library/user-event/dist/utils';
 import React from 'react'
 
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello guys", editableInputTypes);
+    }
+
+    const handleClickMeAgain = (name, e) => {
+        console.log("hello" + name, e.target);
+    }
   return (
     <div className="home">
         <h2>Home Page</h2>
+        <button onClick={handleClick}>Click me</button>
+        <button onClick={(e) => handleClickMeAgain(" hamed", e)}>Click me again</button>
+        
     </div>
   )
 }
